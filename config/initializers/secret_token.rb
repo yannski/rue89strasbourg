b24512b8655bf151f223c1d7872cd7b949cd90ca
Yann Klis <yann.klis@novelys.com>
2012-10-05 20:30:11 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rue89strasbourg::Application.config.secret_token = 'be9fe9bd348b4c3d10cd4adc7e720c82cd024638d9eb02749f3057037261b465ec67dd53d4a16f7a6a0283805fc6374a1c7f24eb5005c525086557fe1260c7b3'
